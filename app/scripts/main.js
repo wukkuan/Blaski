270c4e9ebd28acd9ff0557b2f2d71614a91b5712
William Blasko <williamblasko@gmail.com>
2012-10-23 17:00:57 -0400

    Add ember, ember-data, and handlebars to project and shim it in RequireJS.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,26 @@
 require.config({
   shim: {
+    'handlebars': {
+      exports: 'Handlebars'
+    },
+    'ember': {
+      deps: [ 'jquery', 'handlebars' ],
+      exports: 'Ember'
+    },
+    'ember-data': {
+      deps: [ 'ember' ],
+      exports: 'DS'
+    }
   },
 
   paths: {
-    jquery: 'vendor/jquery.min'
+    'jquery': 'vendor/jquery.min',
+    'ember': 'vendor/ember-latest',
+    'ember-data': 'vendor/ember-data-latest',
+    'handlebars': 'vendor/handlebars-latest'
   }
 });
- 
+
 require(['app'], function(app) {
   // use app here
   console.log(app);
